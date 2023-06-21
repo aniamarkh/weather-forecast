5ade302f3b85897a20c7d052bb3fa133ea71e383
aniamarkh <aniamarkh@gmail.com>
2023-06-21 15:15:59 +0300

    feat: add favicon & page title

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@ import SearchBar from './components/SearchBar.vue';
 </script>
 
 <template>
-  <SearchBar />
+  <main>
+    <h1 class="app__title">
+      <span class="material-symbols-outlined"> routine </span> wow it's a weather app...
+    </h1>
+    <SearchBar />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app__title {
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  cursor: default;
+}
+</style>
